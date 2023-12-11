6548ca3ddffd779c9f55e3cf1bde89998d44c53d
rishar69 <harisharyarevano@gmail.com>
2023-12-11 15:35:46 +0700

    navbar part 2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body >
+
+  <header class="  bg-[#D9D9D9]">
+      <nav>
+        <div">
+           <img class="w-28 mx-28" src="img/logo.png" alt="">
+         </div>
+
+        <div class="">
+            <ul class="flex items-center gap-[4vw]">
+              <li><a href=""> Menu 1 </a></li>
+              <li><a href=""> Menu 2 </a></li>
+              <li><a href=""> Menu 3 </a></li>
+              <li><a href=""> Menu 4 </a></li>
+            </ul>
+        </div>
+        <div>
+          <button class=" px-5 py-2 bg-cyan-500 hover:bg-cyan-600 rounded-full">Sign In</button>
+        </div>
+      </nav>
+  </header>
+
+</body>
+</html>
