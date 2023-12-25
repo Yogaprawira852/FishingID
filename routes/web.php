c252dc18679b57bf1a57d815b9f11158d247161b
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-12-25 13:04:03 +0700

    menambahkan login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/product', function () {
+    return view('product');
+});
+Route::get('/login', function () {
+    return view('login.index');
 });
