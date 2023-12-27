ac75243a5c300aeefe77d6f35b52f8f989da0af5
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-12-27 22:53:11 +0700

    Menambahkan totalSold ditabel item

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,36 +2,47 @@
 @section('container')
     <!-- hero section -->
     <div class="mt-4">
-    <section class="swiper-container mx-10 my-5">
-        <div class="swiper-wrapper">
-            <!-- Slide 1 -->
-            <div class="swiper-slide">
-                <div class="grid lg:grid-cols-2 item bg-center justify-items-center gap-5 font-sans">
-                    <div class="flex flex-col justify-center items-center">
-                        <p class="w-full text-center text-4xl font-extrabold md:text-7xl text-orange-400">Best Choice For Fishing Gear</p>
-                        <p class="w-full text-center text-2xl font-bold md:text-5xl py-3 text-slate-600">100% original</p>
-                        <button class="text-lg md:text-2xl bg-slate-600 text-white hover:bg-slate-500 py-3 px-5 my-5 rounded-md"> Cek Katalog </button>
-                    </div>
-                    <div>
-                        <img class="h-80 w-80 object-cover py-2 px-5 lg:w-[500px] lg:h-[500px] border-r-4 border-slate-400" src="img/hero.png" alt="">
+        <br>
+        <br>
+        <br>
+        <section class="swiper-container mx-10 my-5">
+            <div class="swiper-wrapper">
+                <!-- Slide 1 -->
+                <div class="swiper-slide">
+                    <div class="grid lg:grid-cols-2 item bg-center justify-items-center gap-5 font-sans">
+                        <div class="flex flex-col justify-center items-center">
+                            <p class="w-full text-center text-4xl font-extrabold md:text-7xl text-orange-400">Best Choice For
+                                Fishing Gear</p>
+                            <p class="w-full text-center text-2xl font-bold md:text-5xl py-3 text-slate-600">100% original
+                            </p>
+                            <a href="/login?message=login">
+                                <button
+                                    class="text-lg md:text-2xl bg-slate-600 text-white hover:bg-slate-500 py-3 px-5 my-5 rounded-md">
+                                    Check Our Items
+                                </button>
+                            </a>
+                        </div>
+                        <div>
+                            <img class="h-80 w-80 object-cover py-2 px-5 lg:w-[500px] lg:h-[500px] border-r-4 border-slate-400"
+                                src="img/hero.png" alt="">
+                        </div>
                     </div>
                 </div>
+
+                <!-- Slide 2 (Tambahkan slide lain jika diperlukan) -->
+                <!-- <div class="swiper-slide">
+                                ... (Isi dengan konten slide kedua)
+                            </div> -->
             </div>
 
-            <!-- Slide 2 (Tambahkan slide lain jika diperlukan) -->
-            <!-- <div class="swiper-slide">
-                ... (Isi dengan konten slide kedua)
-            </div> -->
-        </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Add Navigation -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </section>
 
-        <!-- Add Pagination -->
-        <div class="swiper-pagination"></div>
-        <!-- Add Navigation -->
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-    </section>
-
-
+        <hr>
     </div> <!-- kaetegori -->
     <div>
         <div class=" mx-[10%] my-10 border-4 border-orange-400 rounded-lg shadow-md ">
@@ -48,8 +59,7 @@
             </div>
         </div>
     </div>
-
-
+    <hr>
     <!-- Item card -->
     <div class="mx-20 my-10">
         <div class="grid grid-cols-4 justify-items-center gap-2 py-20">
@@ -63,7 +73,8 @@
                             <img class="w-full h-full object-cover" src="img/hero.png" alt="produk">
                             <div class="p-5 flex flex-col gap-3">
                                 <div>
-                                    <span class="px-3 py-1 rounded-full text-xs bg-gray-100">{{ $item->category->name }} </span>
+                                    <span class="px-3 py-1 rounded-full text-xs bg-gray-100">{{ $item->category->name }}
+                                    </span>
                                 </div>
                                 <!-- nama produk -->
                                 <h2 class="font-semibold text-2xl overflow-ellipsis overflow-hidden whitespace-nowrap">
@@ -74,10 +85,12 @@
                                     <span class="text-xl font-bold">Rp.{{ number_format($item->price, 0, ',', '.') }}</span>
                                 </div>
                                 <div class="mt-5 flex gap-2">
-                                    <button class="px-3 py-3 rounded bg-orange-400 hover:bg-orange-300 text-white font-semibold tracking-wider transition">
-                                        Beli Sekarang
+                                    <button
+                                        class="px-3 py-3 rounded bg-orange-400 hover:bg-orange-300 text-white font-semibold tracking-wider transition">
+                                        Buy now
                                     </button>
-                                    <button class="flex-grow flex justify-center items-center bg-gray-400 hover:bg-gray-300 transition rounded-md">
+                                    <button
+                                        class="flex-grow flex justify-center items-center bg-gray-400 hover:bg-gray-300 transition rounded-md">
                                         <i class="fa-solid fa-cart-plus fa-xl" style="color: #000000;"></i>
                                     </button>
                                 </div>
@@ -88,13 +101,13 @@
             @endforeach
             <!-- sampai sini -->
         </div>
-         <!-- Bagian Pagination -->
-         <div class="mt-10" style="text-align: left;">
+        <!-- Bagian Pagination -->
+        <div class="mt-[-3rem]" style="text-align: left;">
             {{ $items->links() }}
         </div>
     </div>
-</div>
-</div>
+    </div>
+    </div>
 
     <script>
         var swiper = new Swiper('.swiper-container', {
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,22 +1,40 @@
-<header class="top-0 fixed w-[100%]  ">
-    <nav class="  bg-slate-300 flex justify-between items-center  mx-auto  rounded px-10  ">
-        <div class="flex flex-row">
-            <a href="/"><img class="w-20 mr-5 py-2" src="img/logo.png" alt=""></a>
-        </div>
+<header class="top-0 fixed w-[100%]">
+        <nav class="bg-slate-300 flex justify-between items-center mx-auto rounded px-10">
+            <div class="flex flex-row">
+                <a href="/"><img class="w-20 mr-5 py-2" src="img/logo.png" alt=""></a>
+            </div>
 
-        <div class="flex w-[55%] h-2/3 rounded mx-5 bg-white">
-            <input type="search" name="search" id="search" placeholder="Mau Cari Apa?"
-                class="w-full border-none bg-transparent px-4 py-1 text-gray-900 outline-none">
-            <button class="m-2 rounded bg-orange-400 hover:bg-orange-300 px-4 py-2 text-white flex"> <i
-                    class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
-        </div>
+            <div class="flex w-[55%] h-2/3 rounded mx-5 bg-white">
+                <input type="search" name="search" id="search" placeholder="What are you looking for?"
+                    class="w-full border-none bg-transparent px-4 py-1 text-gray-900 outline-none">
+                <button id="searchButton"
+                    class="m-2 rounded bg-orange-400 hover:bg-orange-300 px-4 py-2 text-white flex">
+                    <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>
+                </button>
+            </div>
 
-        <div class="">
-            <button class="shadow rounded-lg  hover:bg-slate-200 py-3 px-3 mx-5"><i
-                    class="fa-solid fa-cart-shopping fa-xl " style="color: #4e6b89;"></i></i></button>
-            <a href="/login"
-                class="text-white pl-4  py-3 font-semibold bg-cyan-500 hover:bg-cyan-600 rounded-full">Sign In<i
-                    class=" px-3 py-2 fa-solid fa-arrow-right-to-bracket fa-lg" style="color: #ffffff;"></i></a>
-        </div>
-    </nav>
-</header>
+            <div>
+                <a href="/login?message=login">
+                    <button class="shadow rounded-lg hover:bg-slate-200 py-3 px-3 mx-5 buttonCart">
+                        <i class="fa-solid fa-cart-shopping fa-xl " style="color: #4e6b89;"></i>
+                    </button>
+                </a>
+                <a href="/login" id="loginButton"
+                    class="text-white pl-4 py-3 font-semibold bg-cyan-500 hover:bg-cyan-600 rounded-full">
+                    Sign In<i class="px-3 py-2 fa-solid fa-arrow-right-to-bracket fa-lg" style="color: #ffffff;"></i>
+                </a>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Add your JavaScript script here -->
+    <script>
+        document.getElementById('search').addEventListener('keypress', function(e) {
+            if (e.key === 'Enter') {
+                window.location.href = '/login?message=login';
+            }
+        });
+        document.getElementById('searchButton').addEventListener('click', function() {
+            window.location.href = '/login?message=login';
+        });
+    </script>
